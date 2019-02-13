2759d4a635a76cb09f4300e9a85aa3228a4dedff
Anders Pitman <tapitman11@gmail.com>
2019-02-12 19:18:38 -0700

    gh-pages

--- demo/node/index.js
+++
@@ -1,8 +0,0 @@
-const str2ab = require('string-to-arraybuffer')
-const ab2str = require('arraybuffer-to-string')
-const { initiate } = require('./messend')
-
-const peer = initiate("127.0.0.1", 9001)
-peer.sendMessage(new Uint8Array(str2ab("Hi there from JavaScript")))
-const message = peer.receiveMessageWait()
-console.log("Server responded: " + ab2str(message))
